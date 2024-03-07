edac0acf678bb24ea2f7c510642eb02b0c120875
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-03-07 22:17:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import axios from 'axios';
+
+import AppTitle from './components/AppTitle.vue'
+
+import {store} from './components/store.js'
+export default{
+  data(){
+    return{
+      store,
+    }
+  },
+    components:{
+      AppTitle,
+      
+  },
+  created(){
+   
+   
+  },
+
+  methods: {
+    // chiamata che viene fatta alla pressione del pulsante
+    ChiamataApy(){
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=52d7979549a2ff7c806b67b80cd17c72&query='+ this.store.inputvalue)
+      .then(res =>{
+        // cerco il percorso giusto per array di film
+        console.log('film',res.data.results)
+        // lo salvo all'interno di un array di oggetti
+        this.store.films=res.data.results
+        console.log(this.store.films)
+
+
+      })
+
+    }
+    
+    
+  },
+}
+
+
+</script>
+
+<template>
+  <AppTitle @RicercaPulsante="ChiamataApy"></AppTitle>
+
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,78 @@
+
+<script>
+// importo il mio store
+import {store} from '../components/store.js'
+
+
+export default{
+  name:'AppTitle',
+  data(){
+    return{
+      store,
+      
+      
+      
+
+    }
+  },
+    components:{
+      
+  },
+  created(){
+   
+   
+  },
+
+  methods: {
+    
+    
+  },
+}
+
+
+</script>
+
+<template>
+  <div id="Container_title">
+    <h1>Boolflix</h1>
+    <div id="campo_di_ricerca">
+      <input type="search" placeholder="Ricerca film" v-model="store.inputvalue">
+      <button @click="$emit('RicercaPulsante')">Ricerca</button>
+    </div>
+    
+    
+  </div>
+
+</template>
+
+<style lang="scss">
+#Container_title{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1400px;
+  margin: 0 auto;
+  background-color: #1c1c1c;
+  padding: 15px;
+  h1{
+    text-transform: uppercase;
+    font-size: 40px;
+    color: red;
+  }
+  input{
+    padding: 8px;
+    text-align: center;
+    text-transform: uppercase;
+    border-radius: 8px;
+    border: transparent;
+  }
+  button{
+    height: 100%;
+    padding: 8px;
+    border-radius: 8px;
+    border: transparent;
+    cursor: pointer;
+  }
+}
+
+</style>
