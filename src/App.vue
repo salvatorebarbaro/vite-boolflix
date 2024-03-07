8ff97a5ac81774d3caa94b982a9a5b8f62d37585
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-03-07 22:18:20 +0100

    prima impostazione dell'funzionamento

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,14 @@ export default{
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=52d7979549a2ff7c806b67b80cd17c72&query='+ this.store.inputvalue)
       .then(res =>{
         // cerco il percorso giusto per array di film
+        
         console.log('film',res.data.results)
+        
         // lo salvo all'interno di un array di oggetti
+        
+        
         this.store.films=res.data.results
+
         console.log(this.store.films)
 
 
